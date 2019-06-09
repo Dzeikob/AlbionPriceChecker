1ad10fe48e954d88a241d9b5abe34a9646bd500b
Jakob Joonas <joonas561@hotmail.com>
2019-06-09 08:51:41 +0300

    Add project files.

---
+++ AlbionPriceChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlbionPriceChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
